853c1d46cfa84838c2446574fd765b1194c88cb6
Andres Pluska <pluskaandres@gmail.com>
2024-03-28 15:43:28 -0500

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        battle: resolve(__dirname, "src/battle/index.html"),
+      },
+    },
+  },
+});
